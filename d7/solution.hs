32641d1ae7ecdcfa6fd7408c5f979f0ec56b3ca6
mateusz <matdud915@gmail.com>
2021-12-07 15:28:41 +0000

    refactor d7

--- d7/solution.hs
+++ d7/solution.hs
@@ -1,37 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Text (splitOn, unpack, pack)
+
+import Data.Functor ((<&>))
+import Data.Text (pack, splitOn, unpack)
 
 getInput :: IO String
-getInput = do
-  content <- readFile "input.txt"
-  return $ head (lines content)
+getInput = readFile "input.txt" <&> (head . lines)
 
 parseInput :: String -> [Int]
-parseInput input =
-  let 
-    splitted = splitOn "," (pack input)
-    strSplitted = map unpack splitted
-  in
-    map (read :: String -> Int) strSplitted
+parseInput = map ((read :: String -> Int) . unpack) . splitOn "," . pack
 
 getDistance :: Int -> Int -> Int
 getDistance a b = abs (a - b)
 
-getConsecutiveDistance :: Int -> Int -> Int
-getConsecutiveDistance a b = (distance * (distance + 1)) `div` 2
-  where distance = getDistance a b
+getIncreasingDistance :: Int -> Int -> Int
+getIncreasingDistance a b = (distance * (distance + 1)) `div` 2
+  where
+    distance = getDistance a b
 
 solve :: (Int, Int) -> [Int] -> [Int]
 solve (l, r) xs
-  | l <= r = sum (map (getDistance l) xs) : solve (l+1, r) xs
+  | l <= r = (sum . map (getDistance l)) xs : solve (l + 1, r) xs
   | otherwise = []
 
 solve2 :: (Int, Int) -> [Int] -> [Int]
 solve2 (l, r) xs
-  | l <= r = sum (map (getConsecutiveDistance l) xs) : solve2 (l+1, r) xs
+  | l <= r = (sum . map (getIncreasingDistance l)) xs : solve2 (l + 1, r) xs
   | otherwise = []
 
 main = do
   input <- getInput
   let parsedInput = parseInput input
-  print $ minimum $ solve2 (minimum parsedInput, maximum parsedInput) parsedInput
+  print $ minimum $ solve (minimum parsedInput, maximum parsedInput) parsedInput
+  print $
+    minimum $ solve2 (minimum parsedInput, maximum parsedInput) parsedInput
